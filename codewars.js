53a04b768bed4d529ab5bd26e3a0b07b1f75e911
Kara Bayse <karabayse@gmail.com>
2020-12-26 17:38:36 -0600

    maskify

---
+++ codewars.js
@@ -0,0 +1,16 @@
+/*
+Usually when you buy something, you're asked whether your credit card number,
+phone number or answer to your most secret question is still correct.
+However, since someone could look over your shoulder, you don't want that shown
+on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four
+characters into '#'.
+*/
+// return masked string
+function maskify(cc) {
+  let str = cc.toString();
+  let lastChar = str.slice(-1);
+}
+
+maskify(123456789);
