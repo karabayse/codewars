2bd43b87c59b02812b179dd518d5270178832982
Kara Bayse <karabayse@gmail.com>
2021-01-03 18:03:07 -0600

    array of tiers

--- codewars.js
+++ codewars.js
@@ -34,7 +34,10 @@ descendingOrder(123456789);
 /*
 Create a function that takes a number and returns an array of strings containing
 the number cut off at each digit.
+Example: 420 should return ["4", "42", "420"]
 */
 function createArrayOfTiers(num) {
     return num;
 }
+
+createArrayOfTiers(420);
