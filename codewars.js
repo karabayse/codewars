c22d6b86c660a1fee7850cf3eac0387a83a54184
Kara Bayse <karabayse@gmail.com>
2021-01-09 18:47:52 -0600

    highest and lowest

--- codewars.js
+++ codewars.js
@@ -48,7 +48,8 @@ createArrayOfTiers(420);
 
 // Given a string of space separated numbers, return the highest and lowest number.
 function highAndLow(numbers){
-
+  numbers = numbers.split(" ");
+  return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers);
 }
 
 highAndLow("1 2 3 4 5");  // return "5 1"
