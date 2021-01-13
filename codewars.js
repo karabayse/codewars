b99d1c2919b3db58b3698898380d83c1997c2183
Kara Bayse <karabayse@gmail.com>
2021-01-12 19:24:21 -0600

    bit counting

--- codewars.js
+++ codewars.js
@@ -116,7 +116,8 @@ for (var i = 0; i < array.length; i++) {
 
 
 /*
-Write a function that takes an integer as input, and returns the number of bits
+Write a function that takes an integer as input,
+and returns the number of bits
 that are equal to one in the binary representation of that number.
 You can guarantee that input is non-negative.
 
@@ -124,5 +125,5 @@ Example: The binary representation of 1234 is 10011010010, so the function
 should return 5 in this case
 */
 let countBits = function(n) {
-  // Program Me
+  return (n >>> 0).toString(2);
 };
