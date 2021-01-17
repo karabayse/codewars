bd220671a4949cc66df417e55f50e8b4c5bf5305
Kara Bayse <karabayse@gmail.com>
2021-01-17 17:48:01 -0600

    alphabet position

--- codewars.js
+++ codewars.js
@@ -134,3 +134,17 @@ let countBits = function(n) {
 };
 
 countBits(1234);
+
+
+/*
+Given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+*/
+function alphabetPosition(text) {
+
+}
+
+alphabetPosition("The sunset sets at twelve o' clock.")
+// "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 /*
-given a string, replace every letter with its position in the alphabet.
-
+Given a string, replace every letter with its position in the alphabet.
 If anything in the text isn't a letter, ignore it and don't return it.
 
 "a" = 1, "b" = 2, etc.
