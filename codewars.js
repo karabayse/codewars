6f6065b802d0ca96f4a9bf326396d3248e9c0aea
Kara Bayse <karabayse@gmail.com>
2021-01-19 18:43:12 -0600

    alphabet position

--- codewars.js
+++ codewars.js
@@ -143,7 +143,12 @@ If anything in the text isn't a letter, ignore it and don't return it.
 "a" = 1, "b" = 2, etc.
 */
 function alphabetPosition(text) {
-
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    let code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+  return result.slice(0, result.length - 1);
 }
 
 alphabetPosition("The sunset sets at twelve o' clock.")
