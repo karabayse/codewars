3f5dbe8b8cffd6bb84c68fd978ae722ccb9bccc7
Kara Bayse <karabayse@gmail.com>
2021-01-24 18:39:15 -0600

    human readable seconds

--- test.js
+++ test.js
@@ -8,13 +8,16 @@ SS = seconds, padded to 2 digits, range: 00 - 59
 The maximum time never exceeds 359999 (99:59:59)
 */
 function humanReadable(seconds) {
-  let numyears = Math.floor(seconds / 31536000);
-  let numdays = Math.floor((seconds % 31536000) / 86400);
+  //let numyears = Math.floor(seconds / 31536000);
+  //let numdays = Math.floor((seconds % 31536000) / 86400);
   let numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
   let numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
   let numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
-  return numyears + " years " +  numdays + " days " + numhours + " hours " +
-    numminutes + " minutes " + numseconds + " seconds";
+  return
+    //numyears + " years " +  numdays + " days " +
+    numhours + ":" +
+    numminutes + ":" +
+    numseconds;
 }
 
 humanReadable(1234);
